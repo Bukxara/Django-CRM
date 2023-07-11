161792cd3a7af6f0ace9cdc125c07c3b73dcd2a7
Bukxara <mub805@gmail.com>
2023-07-11 14:14:08 +0500

    changed database config settings

--- bbq/config.py
+++ bbq/config.py
@@ -6,7 +6,7 @@ env.read_env()
 SECRET_KEY = env.str('SECRET_KEY')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-POSTGRES_URL = env.str('POSTGRES_URL')
+
 PGNAME = env.str('PGNAME')
 USER = env.str('PGUSER')
 POSTGRES_PASSWORD = env.str('POSTGRES_PASSWORD')
